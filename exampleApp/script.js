4b2ad16970309c26ab5cd0586207280d1ef263d6
Austen <rawleag@gmail.com>
2015-08-24 14:22:15 -0700

    added more functionality, working on graph function

--- exampleApp/script.js
+++ exampleApp/script.js
@@ -21,19 +21,49 @@ $(function() {
 	});	
 
 	function equals (lcdVal) {
-		var	divide = '\u00F7';
+		// convert the lcdVal into a string which can be evaluated using math.eval
 		var newLcd = '';
+		// tell what to push to newLcd when the character encountered is unknown
+		var	divide = '\u00F7';
+		var pi = '\u03A0';
+		var degrees = '\u00B0';
+		var multiply = '\u00D7';
+		var fx = '\u0192';
+		var equalNotTotal = '\u003D';
 
-			for (var i = 0; i < lcdVal.length; i++){
-				if (lcdVal[i] === divide){
-					newLcd = newLcd + '/';
-				} else if (lcdVal[i] === 'x') {
-					newLcd = newLcd + '*';
-				} else {
-					newLcd = newLcd + lcdVal[i];
-				}
-			}$('#screen').text(math.eval(newLcd));
+		// create the newLcd by looping through each input. Special cases for unknown characters like 
+		// divide, multiply, and fx
+		for (var i = 0; i < lcdVal.length; i++){
+			if (lcdVal[0] === fx) {
+				useFunction(lcdVal);
+			} else if (lcdVal[i] === divide){
+				newLcd = newLcd + '/';
+			} else if (lcdVal[i] === pi) {
+				newLcd = newLcd + 'pi';
+			}else if (lcdVal[i] === degrees) {
+				newLcd = newLcd + '(deg)';
+			}else if (lcdVal[i] === multiply) {
+				newLcd = newLcd + '*';
+			} else {
+				newLcd = newLcd + lcdVal[i];
+			}
+		// output the answer to the string
+		}$('#screen').text(math.eval(newLcd));
 	}
 
+	// if the value of i[0] === var fx (defined in equals()) this function will be run to solve
+	function useFunction (lcdVal) {
+		// require plot.html
+		
+
+
+
+
+	}
 
 });
+
+
+
+
+
