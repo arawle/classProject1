320129a81591996161035b593b71506bbd98691e
Austen <rawleag@gmail.com>
2015-08-24 11:37:17 -0700

    adding in basic calc functionality before new functions)

--- exampleApp/script.js
+++ exampleApp/script.js
@@ -1,5 +1,34 @@
 $(function() {
 
+	$('.buttons').on('click', 'span', function(){
+		console.log(   );
+
+		var lcd = $('#screen').html();
+		var keyVal = $(this).html();
+		var	divide = '\u00F7'
+
+		if ( $(this).attr('id') === 'calc' ) {
+			equals(lcd);
+		} else if ( $(this).attr('id') === 'cancel' ){
+			$('#screen').html(null);
+		} else if ($(this).attr('id') === '=')
+			equals(lcd);
+		else {
+			//displays pushed buttons on screen
+			$('#screen').text( lcd + keyVal );
+		}
+
+	});	
+
+	function equals (lcdVal) {
+		if (keyVal === divide) {
+
+		} else if (keyVal === 'x') {
+
+		} else {
+			$('#screen').text(eval(lcdVal));
+		}
+	}
 
 
 });
