a59fa8c89a1bde729236702eb34d5b18669c0cbb
Austen <rawleag@gmail.com>
2015-08-25 13:06:44 -0700

    working on displaying examples, changed cursor on hover on the Click word

--- blogPost/script.js
+++ blogPost/script.js
@@ -1,5 +1,22 @@
 $(function() {
 
+	// Change cursor when hovering over Click!
+	$('.click4Ex').css('cursor','help')
+
+	// Change exampleDiv to display example when Click! is clicked
+	$('.click4Ex').on('click', function () {
+		alert('hi');
+	})
+
+
+
+// var imgEx = {
+// 	sort: ,
+// 	flatten: ,
+// 	pickRandom: ,
+// 	forEach: ,
+// 	filter: 
+// }
 
 
 
