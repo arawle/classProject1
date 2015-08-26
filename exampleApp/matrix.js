642dfc7a644097e720549c725e399e30b323d545
Austen <rawleag@gmail.com>
2015-08-26 14:50:10 -0700

    got rid of unneccesary console.logs and moved crossProduct variables

--- exampleApp/matrix.js
+++ exampleApp/matrix.js
@@ -3,18 +3,14 @@
 	function matrixIt (lcd, matrixDo) {
 		var toArr = lcd.split(' ');
 
-		var halfOne = toArr[0]
-		var halfTwo = toArr[1]
-		
-		var halfCross = (math.cross(math.eval(halfOne), math.eval(halfTwo)))._data;
-
-		console.log(halfCross)
 		//determine which method to use
 		if (matrixDo === 'determ') {
 			$('#screen').text(math.det(math.eval(toArr)));
 		} else if (matrixDo === 'dotProd') {
 			$('#screen').text(math.eval(lcd));
 		} else if (matrixDo === 'crossProd') {
+			var halfOne = toArr[0]
+			var halfTwo = toArr[1]
 			$('#screen').text('['+(math.cross(math.eval(halfOne), math.eval(halfTwo)))._data + ']');
 		} else {
 			$('#screen').text(math.trace(math.eval(toArr)));
