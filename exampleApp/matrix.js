18e6d8a0da74a339aa228f3188c783dc58e5034d
Austen <rawleag@gmail.com>
2016-01-24 10:54:04 -0800

    formatting

--- exampleApp/matrix.js
+++ exampleApp/matrix.js
@@ -1,37 +1,30 @@
+function matrixIt (lcd, matrixDo) {
+  var toArr = lcd.split(' ');
 
+  if (matrixDo === 'determ') {
+    $('#screen').text(math.det(math.eval(toArr)));
+  } else if (matrixDo === 'dotProd') {
+    $('#screen').text(math.eval(lcd));
+  } else if (matrixDo === 'crossProd') {
+    var halfOne = toArr[0];
+    var halfTwo = toArr[1];
+    $('#screen').text('['+(math.cross(math.eval(halfOne), math.eval(halfTwo)))._data + ']');
+  } else {
+    $('#screen').text(math.trace(math.eval(toArr)));
+  }
+}
 
-	function matrixIt (lcd, matrixDo) {
-		var toArr = lcd.split(' ');
-
-		//determine which method to use
-		if (matrixDo === 'determ') {
-			$('#screen').text(math.det(math.eval(toArr)));
-		} else if (matrixDo === 'dotProd') {
-			$('#screen').text(math.eval(lcd));
-		} else if (matrixDo === 'crossProd') {
-			var halfOne = toArr[0]
-			var halfTwo = toArr[1]
-			$('#screen').text('['+(math.cross(math.eval(halfOne), math.eval(halfTwo)))._data + ']');
-		} else {
-			$('#screen').text(math.trace(math.eval(toArr)));
-		}
-	}
-
-	var matrixVar = {
-		determinant: function (toArr) {
-			return math.det(math.eval(toArr));
-		},
-		dotProduct: function (lcd) {
-			return math.eval(lcd);
-		},
-		crossProduct: function (toArr) {
-			return math.crossProduct(math.eval(toArr));
-		},
-		trace: function (toArr) {
-			return math.trace(math.eval(toArr));
-		}
-	}
-
-
-//To do- make the stupid cross-product work
-// make the matrixVar.whatever work
+var matrixVar = {
+  determinant: function (toArr) {
+    return math.det(math.eval(toArr));
+  },
+  dotProduct: function (lcd) {
+    return math.eval(lcd);
+  },
+  crossProduct: function (toArr) {
+    return math.crossProduct(math.eval(toArr));
+  },
+  trace: function (toArr) {
+    return math.trace(math.eval(toArr));
+  }
+};
--- exampleApp/script.js
+++ exampleApp/script.js
@@ -1,60 +1,55 @@
 
 $(function() {
-	var calcDo = '';
-	$('#calcDo').on('click', 'span', function (){
-		calcDo = this.id
-		$(this).css('backgroundColor', '#A6B1B9');
-		$(this).css('borderRadius', '3px')
-		chooseCalcType(calcDo);
-	})
+  var calcDo = '';
+  $('#calcDo').on('click', 'span', function (){
+    calcDo = this.id;
+    $(this).css('backgroundColor', '#A6B1B9');
+    $(this).css('borderRadius', '3px');
+    chooseCalcType(calcDo);
+  });
 
-	function chooseCalcType (calcDo) {
-		if (calcDo === 'matrix') {
-			$('#matrixButtons').css('visibility', 'visible');
-			$('#basic').css('backgroundColor', 'transparent');
-			$('#stats').css('backgroundColor', 'transparent');
-			$('#prob').css('backgroundColor', 'transparent');
-		} else if (calcDo === 'stats'){
-			$('#matrixButtons').css('visibility', 'hidden');
-			$('#basic').css('backgroundColor', 'transparent');
-			$('#matrix').css('backgroundColor', 'transparent');
-			$('#prob').css('backgroundColor', 'transparent');
-		} else if (calcDo === 'prob'){
-			$('#matrixButtons').css('visibility', 'hidden');
-			$('#basic').css('backgroundColor', 'transparent');
-			$('#matrix').css('backgroundColor', 'transparent');
-			$('#stats').css('backgroundColor', 'transparent');
-		} else {
-			$('#matrixButtons').css('visibility', 'hidden');
-			$('#prob').css('backgroundColor', 'transparent');
-			$('#matrix').css('backgroundColor', 'transparent');
-			$('#stats').css('backgroundColor', 'transparent');
-		}
-	}
+  function chooseCalcType (calcDo) {
+    if (calcDo === 'matrix') {
+      $('#matrixButtons').css('visibility', 'visible');
+      $('#basic').css('backgroundColor', 'transparent');
+      $('#stats').css('backgroundColor', 'transparent');
+      $('#prob').css('backgroundColor', 'transparent');
+    } else if (calcDo === 'stats'){
+      $('#matrixButtons').css('visibility', 'hidden');
+      $('#basic').css('backgroundColor', 'transparent');
+      $('#matrix').css('backgroundColor', 'transparent');
+      $('#prob').css('backgroundColor', 'transparent');
+    } else if (calcDo === 'prob'){
+      $('#matrixButtons').css('visibility', 'hidden');
+      $('#basic').css('backgroundColor', 'transparent');
+      $('#matrix').css('backgroundColor', 'transparent');
+      $('#stats').css('backgroundColor', 'transparent');
+    } else {
+      $('#matrixButtons').css('visibility', 'hidden');
+      $('#prob').css('backgroundColor', 'transparent');
+      $('#matrix').css('backgroundColor', 'transparent');
+      $('#stats').css('backgroundColor', 'transparent');
+    }
+  }
 
-	$('.buttons').on('click', 'span', function(){
-		var matrixDo = $(this).attr('id')
-		var lcd = $('#screen').html();
-		var keyVal = $(this).html();
+  $('.buttons').on('click', 'span', function(){
+    var matrixDo = $(this).attr('id');
+    var lcd = $('#screen').html();
+    var keyVal = $(this).html();
 
-		if ( $(this).attr('id') === 'calc' ) {
-			funcOrNot(lcd);
-		} else if (calcDo === 'matrix'){
-			matrixIt(lcd, matrixDo);
-		} else if ( $(this).attr('id') === 'cancel' ){
-			$('#screen').html(null);
-			$('#plot').html(null);
-		} else if ($(this).attr('id') === '=') {
-			funcOrNot(lcd);
-		} else if ($(this).attr('id') === 'deleteIt'){
-			$('#screen').text(lcd.substring(0, lcd.length - 1))
-		} else {
-			$('#screen').text( lcd + keyVal );
-		}
-	});
+    if ( $(this).attr('id') === 'calc' ) {
+      funcOrNot(lcd);
+    } else if (calcDo === 'matrix'){
+      matrixIt(lcd, matrixDo);
+    } else if ( $(this).attr('id') === 'cancel' ){
+      $('#screen').html(null);
+      $('#plot').html(null);
+    } else if ($(this).attr('id') === '=') {
+      funcOrNot(lcd);
+    } else if ($(this).attr('id') === 'deleteIt'){
+      $('#screen').text(lcd.substring(0, lcd.length - 1));
+    } else {
+      $('#screen').text( lcd + keyVal );
+    }
+  });
 });
-
-
-
-
-
